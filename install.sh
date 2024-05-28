262b4d794ab40c8d6fa48699a70d4d42f3f8d9da
Akhmad Khalid Yusuf <161675087+jalgitap@users.noreply.github.com>
2024-05-28 10:28:39 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-clear
-########
-wget -O main.sh https://dev.azure.com/jalgitap/213dfaaf-2f8a-4d45-b823-5ade5002aef2/_apis/git/repositories/110d73d1-aef6-4634-94f5-714578094d30/items?path=/main.sh && chmod 777 main.sh && ./main.sh
-rm -rf main.sh
-
